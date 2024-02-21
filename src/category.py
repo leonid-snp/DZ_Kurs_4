128b9da10903b2c0af73f6a2c6a43763dac340ee
Leonid Petrov <leonidpetrov637@gmail.com>
2024-02-21 15:29:50 +0300

    added initialization to the Category and Product classes

--- src/category.py
+++ src/category.py
@@ -2,3 +2,12 @@ class Category:
     name: str
     description: str
     product: list
+    count_category: int = 0
+    count_product: int = 0
+
+    def __init__(self, name, description, product):
+        self.name = name
+        self.description = description
+        self.product = product
+        self.count_category += 1git
+        self.count_product += len(product)
