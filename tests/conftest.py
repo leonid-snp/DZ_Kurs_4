f25a3edf9ea970ca053d34060c4040a1577e92be
Leonid Petrov <leonidpetrov637@gmail.com>
2024-02-22 15:36:17 +0300

    Reworked the additional task loading via json file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from src.category import Category
 from src.product import Product
+from utils.pars_json import creates_instance_class
 
 
 @pytest.fixture
@@ -11,3 +12,8 @@ def test_category():
 @pytest.fixture
 def test_product():
     return Product("Samsung", "Топ смартфон", 80_000.0, 7)
+
+
+@pytest.fixture
+def test_pars_json():
+    return creates_instance_class()
