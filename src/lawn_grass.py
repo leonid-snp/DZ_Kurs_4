0055672e54e9d4fd00dc9b63fda08a267cb45181
Leonid Petrov <leonidpetrov637@gmail.com>
2024-02-26 11:41:13 +0300

    Added an abstract class described methods inherited from Product, Smartphone, LawnGrass

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -1,7 +1,8 @@
+from src.abstract_product import AbstractProduct
 from src.product import Product
 
 
-class LawnGrass(Product):
+class LawnGrass(Product, AbstractProduct):
     """
     Класс Трава Газонная
 
@@ -13,7 +14,20 @@ class LawnGrass(Product):
     germination_period (float) срок прорастания
     color (str) цвет травы
     """
-    def __init__(self, name: str, description: str, price: float, quantity: int, manufacturer: str, germination_period: float, color: str):
+    def __init__(self, name: str, description: str, price: float, quantity: int, manufacturer: str,
+                 germination_period: float, color: str):
         super().__init__(name, description, price, quantity, color)
         self.manufacturer = manufacturer
         self.germination_period = germination_period
+
+    @classmethod
+    def creates_product(cls, product: dict) -> object:
+        """
+        Класс метод который принимает словарь и создает новый объект класса Product
+
+        :product (dict) продукт по которому создаст объект
+
+        :return (object) объект класса Product
+        """
+        return cls(product["name"], product["description"], product["price"], product["quantity"],
+                   product["manufacturer"], product["germination_period"], product["color"])
